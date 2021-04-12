543b9b888c1e73ad551643bbcff920ac8f8868de
Micah <mrusher@gmail.com>
2021-04-11 21:26:12 -0500

    Lesson 1 exercises complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'The Internet Was Built On Cats';
+  image1 = 'https://filmdaily.co/wp-content/uploads/2020/07/if-it-fits-lede-1300x1123.jpg';
+  image2 = 'https://www.zenrooms.com/blog/wp-content/uploads/2020/05/funny-cats-2-1280x720.jpg';
+  image3 = 'https://i1.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg';
 
   constructor() { }
 
